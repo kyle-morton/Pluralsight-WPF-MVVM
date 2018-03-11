946a16ebcac387e38a4b8d465fbf558fce0316ea
kylemorton5770 <kylemortondevelopment@gmail.com>
2018-03-10 21:58:44 -0600

    added custom behavior from mainWindow to mainWindowViewModel

--- ZzaDashboard/ZzaDashboard/MainWindowViewModel.cs
+++ ZzaDashboard/ZzaDashboard/MainWindowViewModel.cs
@@ -1,19 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using ZzaDashboard.Customers;
 
 namespace ZzaDashboard
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
+
+        #region PROPS
+
+        #region event handlers
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        #endregion
+
+        private Timer _timer = new Timer(5000);
         public object CurrentViewModel { get; set; }
+
+        private string _notificationMessage;
+        public string NotificationMessage
+        {
+            get
+            {
+                return _notificationMessage;
+            }
+            set
+            {
+                if (_notificationMessage != value)
+                {
+                    _notificationMessage = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("NotificationMessage"));
+                }
+            }
+        }
+
+        #endregion
 
         public MainWindowViewModel()
         {
             CurrentViewModel = new CustomerListViewModel();
+            _timer.Elapsed += (s, e) =>
+            {
+                NotificationMessage = "At the tone, the time will be: " + DateTime.Now.ToLocalTime().ToString();
+            };
+            _timer.Start();
         }
+
     }
 }
---
+++ ZzaDashboard/ZzaDashboard/ShowNotificationMessageBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace ZzaDashboard
+{
+    public class ShowNotificationMessageBehavior : Behavior<ContentControl>
+    {
+
+        #region DEP PROPS
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Message.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(ShowNotificationMessageBehavior), new PropertyMetadata(null, OnMessageChanged));
+
+        #endregion
+
+        #region EVENTS
+
+        private static void OnMessageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //cast incoming behaviro to ShowNotificationMessageBehavior
+            var behavior = ((ShowNotificationMessageBehavior)d);
+            behavior.AssociatedObject.Content = e.NewValue;
+            behavior.AssociatedObject.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// on associating this class w/ incoming behavior, 
+        /// attach click handler to dismiss message
+        /// </summary>
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += (s, e) =>
+            {
+                AssociatedObject.Visibility = Visibility.Collapsed;
+            };
+        }
+
+        #endregion
+
+
+    }
+}
