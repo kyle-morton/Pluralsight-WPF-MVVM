32f8315ad0b5969f6d16e65df4317032e10952ea
Kyle Morton <kylemortondevelopment@gmail.com>
2018-03-18 15:42:36 -0500

    new ZzaDesktop.UI wpf app for last 2 modules, created VM-driven nav system w/ commands

--- ZzaDashboard/ZzaDashboard.Shared/BindableBase.cs
+++ ZzaDashboard/ZzaDashboard.Shared/BindableBase.cs
@@ -10,15 +10,31 @@ namespace ZzaDashboard.Shared
 {
     public class BindableBase : INotifyPropertyChanged
     {
+
+        /// <summary>
+        /// triggers on property changed event for property if value has changed. 
+        /// Uses C# CallerMemberName to get property name in the setter for which method was called in.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="member"></param>
+        /// <param name="val"></param>
+        /// <param name="propertyName"></param>
         protected virtual void SetProperty<T>(ref T member, T val,
             [CallerMemberName] string propertyName = null)
         {
+
+            //checks if member variable and new value are same
             if (object.Equals(member, val)) return;
 
+            //otherwise, sets new value and triggers prop changed event
             member = val;
             PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        /// <summary>
+        /// used to invoke PropertyChanged event for property w/ given name
+        /// </summary>
+        /// <param name="propertyName"></param>
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
---
+++ ZzaDashboard/ZzaDesktop.UI/MainWindowViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZzaDashboard.Shared;
+using ZzaDesktop.UI.Customers;
+using ZzaDesktop.UI.OrderPrep;
+using ZzaDesktop.UI.Orders;
+
+namespace ZzaDesktop.UI
+{
+    public class MainWindowViewModel : BindableBase
+    {
+
+        #region PROPS
+
+        #region view models
+
+        private CustomerListViewModel _customerListViewModel = new CustomerListViewModel();
+        private OrderPrepViewModel _orderPrepViewModel = new OrderPrepViewModel();
+        private OrderViewModel _orderViewModel = new OrderViewModel();
+
+        private BindableBase _currentViewModel;
+        public BindableBase CurrentViewModel
+        {
+            get
+            {
+                return _currentViewModel;
+            }
+            set
+            {
+                SetProperty(ref _currentViewModel, value);
+            }
+        }
+
+        #endregion
+
+        #region commands
+
+        public RelayCommand<string> NavCommand { get; private set; }
+
+        #endregion
+
+        #endregion
+
+        #region CTOR
+
+        public MainWindowViewModel()
+        {
+            //set NavCommand to call OnNav method
+            NavCommand = new RelayCommand<string>(OnNav);
+        }
+
+        #endregion
+
+        #region NAVIGATION
+
+        /// <summary>
+        /// sets current view model based on destination string passed
+        /// </summary>
+        /// <param name="destination"></param>
+        private void OnNav(string destination)
+        {
+            switch(destination)
+            {
+                case "orderPrep":
+                    CurrentViewModel = _orderPrepViewModel;
+                    break;
+                case "customers":
+                default:
+                    CurrentViewModel = _customerListViewModel;
+                    break;
+            }
+        }
+
+        #endregion
+
+
+    }
+}
